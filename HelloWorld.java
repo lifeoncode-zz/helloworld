e07e70acde44c49c80af24efdb3eb6a4a567316d
octmoyo021 <octmoyo021@student.wethinkcode.co.za>
2021-12-21 11:33:24 +0200

    added a multi-line comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 // THIS IS A COMMENT
 
+/* THIS IS A MULTI-LINE COMMENT 
+ANY TEXT BETWEEN THE ASTERISKS AND THE FORWARD SLASH 
+IS KNOWN AS A MULTI-LINE COMMENT*/
 
 public class HelloWorld {
     public static void main(String[] args) {
